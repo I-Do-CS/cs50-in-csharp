e247d6205ba45fc5b457340043af44dd37991279
I-Do-CS <150284763+I-Do-CS@users.noreply.github.com>
2024-06-01 14:44:42 +0330

    Add "hello, it's me" assignment

---
+++ Hello/Hello/Hello/Program.cs
@@ -0,0 +1,27 @@
+﻿// https://cs50.harvard.edu/x/2024/psets/1/me/
+using System.Globalization;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("What's Your Name? ");
+            string? name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Wrong Input, Try Again.");
+                Main();
+                return;
+            
+            } 
+
+            TextInfo nameInfo = new CultureInfo("en-US", false).TextInfo;
+            name = nameInfo.ToTitleCase(name.Trim().ToLower());
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
