099001d342f65adc06af354c2e6609a57693f2ca
I-Do-CS <150284763+I-Do-CS@users.noreply.github.com>
2024-06-03 11:15:51 +0330

    Add mario-more

---
+++ MarioMore/MarioMore/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MarioMore
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Pyramid Height: ");
+            string? heightInput = Console.ReadLine();
+
+            int height;
+            try
+            {
+                height = Convert.ToInt32(heightInput);
+
+                if (height > 10) 
+                {
+                    throw new FormatException();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Height invalid or bigger than 10, try again\n");
+                Main();
+                return;
+            }
+
+            PrintPyramid(height);
+        }
+
+        static void PrintPyramid(int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                Console.Write( new string(' ', height - i - 1) );
+                Console.Write( new string('#', i + 1) );
+                Console.Write("  ");
+                Console.WriteLine(new string('#', i + 1));
+
+            }
+        }
+    }
+}
